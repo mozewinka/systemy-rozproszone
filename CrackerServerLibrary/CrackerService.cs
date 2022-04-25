5edcc6ddaf0cc2394cb05b8d3ae44a0b619bc0bc
Piotrek <piotrek.piotr2@gmail.com>
2022-04-25 23:41:14 +0200

    Add project files.

---
+++ CrackerServerLibrary/CrackerService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CrackerServerLibrary
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, AddressFilterMode = AddressFilterMode.Any)]
+    public class CrackerService : ICrackerService
+    {
+        public void Receive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Send()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CrackerServerLibrary/ICrackerService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CrackerServerLibrary
+{
+    [ServiceContract(SessionMode = SessionMode.Required, CallbackContract = typeof(ICrackerServiceCallback))]
+    public interface ICrackerService
+    {
+        [OperationContract(IsOneWay = true)]
+        void Send();
+
+        [OperationContract(IsOneWay = true)]
+        void Receive();
+
+    }
+    public interface ICrackerServiceCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void Print();
+
+    }
+}
