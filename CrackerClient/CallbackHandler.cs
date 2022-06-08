add8b028f1f862712bd694f580c4c2113f775ebb
unknown <weronika.wolszczak@gmail.com>
2022-06-09 01:34:47 +0200

    zapis do pliku wyniku pomiaru czasowego

--- CrackerClient/CallbackHandler.cs
+++ CrackerClient/CallbackHandler.cs
@@ -3,6 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.ServiceModel;
 using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Linq;
+
 
 namespace CrackerClient
 {
@@ -16,6 +20,18 @@ namespace CrackerClient
         {
             DictionaryList = new List<string>();
         }
+        public void IO(string perf) //funkcja obslugujaca zapis do pliku
+        {
+            string filePath = @"C:\Users\weron\source\repos\rozproszone_proj\systemy-rozproszone\CrackerClient\TimesResults.txt"; //sciezka do pliku
+
+            List<string> lines = new List<string>();
+
+            lines = File.ReadAllLines(filePath).ToList();
+
+            lines.Add(perf);
+            File.WriteAllLines(filePath, lines);
+        }
+
 
         public void Print(string md5Password)
         {
@@ -49,9 +65,11 @@ namespace CrackerClient
 
             stopwatch.Stop();
             string elapsed = ". Elapsed time: " + stopwatch.ElapsedMilliseconds + " ms";
-            string performance = ". Average cracking speed was " + counter / stopwatch.ElapsedMilliseconds * 1000 + " H/s";
+            string performance = result + ". Average cracking speed was " + counter / stopwatch.ElapsedMilliseconds * 1000 + " H/s"; //jest ilosc powtorzen przez czas jako sredni czas a wydaje mi sie ze powinno byc na odwrot xd ze sredni czas to calkowity czas / ilosc powtorzen ;x
             Console.WriteLine(result + elapsed + performance);
             Client.AnnounceResult(result + elapsed + performance);
+            IO(performance); //zapis
+
         }
 
         public void DictionaryCrack(int startPosition, int endPosition, string md5Password, bool checkUpperCase, bool checkSuffix, string suffix)
@@ -134,9 +152,10 @@ namespace CrackerClient
 
             stopwatch.Stop();
             string elapsed = ". Elapsed time: " + stopwatch.ElapsedMilliseconds + " ms";
-            string performance = ". Average cracking speed was " + counter / stopwatch.ElapsedMilliseconds * 1000 + " H/s";
+            string performance = result + ". Average cracking speed was " + counter / stopwatch.ElapsedMilliseconds * 1000 + " H/s"; //dopisalam zeby zapisywalo w pliku tez wynik crackowania i czy sie udalo
             Console.WriteLine(result + elapsed + performance);
             Client.AnnounceResult(result + elapsed + performance);
+            IO(performance); //funckja zapisu
         }
     }
 }
